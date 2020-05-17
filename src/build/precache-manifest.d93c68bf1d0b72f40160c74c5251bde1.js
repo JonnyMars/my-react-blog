8eae2f019d1a645c180901e50f0e03d970febaf8
Jonny <jorfn@hotmail.co.uk>
2020-05-17 18:58:18 +0100

    Initial Commit

---
+++ src/build/precache-manifest.d93c68bf1d0b72f40160c74c5251bde1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7095386c4c691a419c1b671fc67f1ed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249a52828cc00889a784",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "776c396d3bbf9458d701",
+    "url": "/static/js/2.54eff4ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54eff4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249a52828cc00889a784",
+    "url": "/static/js/main.1acea907.chunk.js"
+  },
+  {
+    "revision": "80ddaeb08143afaca73f",
+    "url": "/static/js/runtime-main.0e47f4f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
